5d58d87d0470d44de19a7137b84d5517a215a613
Cory Keith <38738928+CoryKeith@users.noreply.github.com>
2018-05-01 01:57:14 -0400

    Updated Navbar
    Navbar now has the correct links instead of placeholders.

--- navbar.php
+++ navbar.php
@@ -4,23 +4,14 @@
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav mx-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li> 
-      <li class="nav-item"><a class="active" href="Bootstrap_Index.php">Home</a></li>
-		  <li class="nav-item"><a href="staff.php">Staff</a></li>
-		  <li class="nav-item"><a href="contact.php">Contact Us</a></li>
-		  <li class="nav-item"><a href="tour.php">Office Tour</a></li>
-		  <li class="nav-item"><a href="about.php">About</a></li>
-	  	<li class="nav-item"><a href="services.php">Services</a></li>
-		  <li class="nav-item"><a href="resources.php">Patient Resources</a></li>   
+    <ul class="navbar-nav mx-auto"> 
+      <li class="nav-item"><a class="nav-link" href="Bootstrap_Index.php">Home</a></li>
+		  <li class="nav-item"><a class="nav-link" href="staff.php">Staff</a></li>
+		  <li class="nav-item"><a class="nav-link" href="contact.php">Contact Us</a></li>
+		  <li class="nav-item"><a class="nav-link" href="tour.php">Office Tour</a></li>
+		  <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
+	  	<li class="nav-item"><a class="nav-link" href="services.php">Services</a></li>
+		  <li class="nav-item"><a class="nav-link" href="resources.php">Patient Resources</a></li>
     </ul>
   </div>  
 </nav>
