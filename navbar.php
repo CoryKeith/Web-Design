825722c2ba719a7493ba41d170e7ce7eb79ccab4
Cory Keith <38738928+CoryKeith@users.noreply.github.com>
2018-05-01 00:45:50 -0400

    fixed navbar with 4.0
    Fixed the Navbar so that it works with Bootstrap 4.0 and added some padding between the links on the navbar.

--- navbar.php
+++ navbar.php
@@ -1,32 +1,10 @@
-<!-- <nav class="navbar navbar-expand-md bg-light navbar-light navbar-fixed-top">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav navbar-center">
-        <li><a href="Bootstrap_Index.php">Home</a></li>
-		    <li><a href="staff.php">Staff</a></li>
-		    <li><a href="contact.php">Contact Us</a></li>
-	    	<li><a href="tour.php">Office Tour</a></li>
-	    	<li><a href="about.php">About</a></li>
-	     	<li><a href="services.php">Services</a></li>
-    		<li><a href="resources.php">Patient Resources</a></li>
-      </ul>
-    </div>
-  </div>
-</nav> -->
-<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+<nav class="navbar navbar-expand-md bg-light navbar-light navbar-fixed-top">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav">
+    <ul class="navbar-nav mx-auto">
       <li class="nav-item">
         <a class="nav-link" href="#">Link</a>
       </li>
